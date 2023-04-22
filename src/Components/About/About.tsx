3dbc4c7bb5004a1460de3911a67af4b6d4571ae3
Tosin Mayowa <toss800@gmail.com>
2023-04-22 17:55:51 +0100

    complete about section

---
+++ src/Components/About/About.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import mayowa from "../../Assets/mayowa.jpg"; 
+import './About.scss';
+const About=()=>{
+
+    return (
+      <>
+        <section className="about__me">
+          <div className="about__me-img">
+            <img src={mayowa} alt="mayowa" />
+          </div>
+          <div className="about__me-content">
+            <h2 className="about__me-content--title">About me</h2>
+            <p className="about__me-content--para">
+              Frontend Engineer with experience in building solutions to solve
+              real life problems. Design, build and implement the user interface
+              systems of web based applications achieving a satisfactory user
+              experience with no issues, errors or downtime, using React.js,
+              Next.js,Typescript,Redux ,React Testing Library and Jest, Git, GIT
+              CI/CD, Chakra-Ui, React-Table. I have experience working in an
+              Agile environment using tools like Jira and Slack. I also have
+              skills when it comes to leading and managing team and people, an
+              experience I gathered during my 10years in teaching.I graduated
+              at the top of my class with Distinction. As a good Mathematics
+              student, I love solving complex problems, When it comes to web
+              page functionality and design, I am your guy.
+            </p>
+            <a href="" className="about__me-content--link">
+                Read more
+            </a>
+          </div>
+        </section>
+      </>
+    );
+}
+
+export default About;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import withLayout from '../withLayout';
 import HomeComp from '../Components/Home/HomeComp';
 import Project from '../Components/Project/Project';
+import About from '../Components/About/About';
 const Home=()=>{
 
     return(
         <>
 <HomeComp/>
 <Project/>
+<About/>
         </>
     )
 }
