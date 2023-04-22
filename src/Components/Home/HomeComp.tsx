6de337628f5417b5ad80a732375bc4a440d70fed
Tosin Mayowa <toss800@gmail.com>
2023-04-22 06:13:05 +0100

    complete home page

---
+++ src/Components/Home/HomeComp.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './Home.scss'
+const HomeComp=()=>{
+
+    return(
+        <>
+<section id="home">
+<div className="home__content">
+  <h1 className="home__content-title">
+      Mayowa Tosin Adejumola
+  </h1>
+  <p className="home__content-para">
+      Frontend Engineer
+  </p>
+  <a href="" className="home__content-link">My Project</a>
+</div>
+</section>
+        </>
+    )
+}
+
+export default HomeComp;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import withLayout from '../withLayout';
+import HomeComp from '../Components/Home/HomeComp'
 const Home=()=>{
 
     return(
         <>
-<div>
-    Home
-</div>
+<HomeComp/>
         </>
     )
 }
