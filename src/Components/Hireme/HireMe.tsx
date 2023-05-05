6cf4662790b1011f322047a937f812f3256f3f10
Tosin Mayowa <toss800@gmail.com>
2023-05-05 11:43:51 +0100

    hire me in progress

---
+++ src/Components/Hireme/HireMe.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HireMe=()=>{
+
+    return (
+        <>
+<div>Hire me page still work in progress</div>
+        </>
+    )
+}
+
+export default HireMe;
---
+++ src/pages/hireMe.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import withLayout from "../withLayout";
+import HireMe from "../Components/Hireme/HireMe";
+const HireMePage = () => {
+  return (
+    <>
+      <HireMe/>
+    </>
+  );
+};
+export default withLayout(HireMePage);
