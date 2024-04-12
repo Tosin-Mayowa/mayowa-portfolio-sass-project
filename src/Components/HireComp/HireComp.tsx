b33493d3c4cf3ff11421188033fe3f307a31bfe1
Tosin Mayowa <toss800@gmail.com>
2024-04-12 07:21:20 +0100

    hire me update

--- src/Components/HireComp/HireComp.tsx
+++ src/Components/HireComp/HireComp.tsx
@@ -14,7 +14,7 @@ import "./HireComp.scss";
             projects are displayed in the project section, you can click on the
             images to view the live projects. I worked on several areas of an
             application that requires in depth knowledge of the my stack, areas
-            like authetication, integrating third pirty library like
+            like creating Rest web api,improving the performance of web api(using in-memory caching, optimizing database queries,using DTOs to transfer required data etc),handling application security (authetication, authorisation, CORS,CSP) both on serverside and view side of the source code, integrating third party library like
             googleApi, converting figma design to code,consuming API from the backend,handling errors on frontend and backend,creating middlewares to intercept request before hitting the server, creating custom reusable component.
           </p>
         </div>
